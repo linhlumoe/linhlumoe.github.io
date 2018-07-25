88a42969d986a1277315dcf7c43a5613bb2937ba
kylie <khanhlinh970@gmail.com>
2018-07-26 00:50:36 +0700

    add animation to home section

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var home = document.getElementById('greeting--content');
+// var parallaxInstance = new Parallax(home, {
+//   // relativeInput: true
+// });
+
+// parallaxInstance.friction(0.8, 0.8);
+// parallaxInstance.invert(0.2, 0.2);
