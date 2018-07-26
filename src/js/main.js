052d6ce5cfb80d8dad6adec6a51daae497824efa
Kylie Tran <khanhlinh970@gmail.com>
2018-07-26 18:14:10 +0700

    add tweenmax and scrollmagic

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,77 @@
-var home = document.getElementById('greeting--content');
-// var parallaxInstance = new Parallax(home, {
-//   // relativeInput: true
-// });
+$(function() {
+  // Init Controller
+  var controller = new ScrollMagic.Controller();
 
-// parallaxInstance.friction(0.8, 0.8);
-// parallaxInstance.invert(0.2, 0.2);
+  // Parallax background
+  new ScrollMagic.Scene({
+    triggerElement: "#home--background",
+    triggerHook: "onEnter",
+  })
+    .duration('200%')
+    .setTween("#background-video", {
+      backgroundPosition: "80% 100%",
+      ease: Linear.easeNone
+    })
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+  // Slide in
+  new ScrollMagic.Scene({
+    triggerElement: "#slidein",
+    triggerHook: "onLeave",
+  })
+    .setPin("#slidein")
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#slidein2",
+    triggerHook: "onLeave",
+  })
+    .setPin("#slidein2")
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+  //Moving divs
+  var fromLeftTimeline = new TimelineMax();
+  var fromLeftFrom = TweenMax.from("#opacity", 1, {
+    y: 500
+  });
+  var fromLeftTo = TweenMax.to("#opacity", 1, {
+    y: 0
+  });
+  fromLeftTimeline
+    .add(fromLeftFrom)
+    .add(fromLeftTo);
+
+  var fromBottomTimeline = new TimelineMax();
+  var fromBottomFrom = TweenMax.from("#bottom", 1, {
+    y: 500
+  });
+  var fromBottomTo = TweenMax.to("#bottom", 1, {
+    y: 0
+  });
+  fromBottomTimeline
+    .add(fromBottomFrom)
+    .add(fromBottomTo);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#slidein2",
+    offset: 100,
+  })
+    .setTween(fromLeftTimeline)
+    .duration(400)
+    //    .reverse(false)
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "#slidein2",
+    offset: 100
+  })
+    .setTween(fromBottomTimeline)
+    .duration(800)
+    //    .reverse(false)
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+});
